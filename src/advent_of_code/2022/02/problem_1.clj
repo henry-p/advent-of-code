1f1946975f847519dc4113a6951044b286586806
Henry Perschk <henry.perschk@gmail.com>
2022-12-02 07:49:04 +0100

    2022/02

--- src/advent_of_code/2022/02/problem_1.clj
+++ src/advent_of_code/2022/02/problem_1.clj
@@ -4,7 +4,42 @@
 
 (def parsed
   (->> (slurp "./resources/advent_of_code/2022/day_02.txt")
-       str/split-lines))
+       str/split-lines
+       (map #(str/split % #" "))))
 
+(def opp-lookup
+  {"A" :rock
+   "B" :paper
+   "C" :scissors})
 
+(def me-lookup
+  {"X" :rock
+   "Y" :paper
+   "Z" :scissors})
 
+(def points-outcome
+  {[:rock :rock] 3
+   [:rock :paper] 6
+   [:rock :scissors] 0
+   [:paper :rock] 0
+   [:paper :paper] 3
+   [:paper :scissors] 6
+   [:scissors :rock] 6
+   [:scissors :paper] 0
+   [:scissors :scissors] 3})
+
+(def points-per-type
+  {:rock 1
+   :paper 2
+   :scissors 3})
+
+(defn calc-outcome [input]
+  (let [[opp me] input
+        opp-type (opp-lookup opp)
+        me-type (me-lookup me)
+        battle [opp-type me-type]
+        battle-points (points-outcome battle)
+        me-points (points-per-type me-type)]
+    (+ battle-points me-points)))
+
+(apply + (map calc-outcome parsed))
--- src/advent_of_code/2022/02/problem_2.clj
+++ src/advent_of_code/2022/02/problem_2.clj
@@ -4,7 +4,56 @@
 
 (def parsed
   (->> (slurp "./resources/advent_of_code/2022/day_02.txt")
-       str/split-lines))
+       str/split-lines
+       (map #(str/split % #" "))))
 
+(def opp-lookup
+  {"A" :rock
+   "B" :paper
+   "C" :scissors})
 
+(def need-to
+  {"X" :loose
+   "Y" :draw
+   "Z" :win})
 
+(def me-chooser
+  {[:rock :loose] :scissors
+   [:rock :draw] :rock
+   [:rock :win] :paper
+   [:paper :loose] :rock
+   [:paper :draw] :paper
+   [:paper :win] :scissors
+   [:scissors :loose] :paper
+   [:scissors :draw] :scissors
+   [:scissors :win] :rock})
+
+(def points-outcome
+  {[:rock :rock] 3
+   [:rock :paper] 6
+   [:rock :scissors] 0
+   [:paper :rock] 0
+   [:paper :paper] 3
+   [:paper :scissors] 6
+   [:scissors :rock] 6
+   [:scissors :paper] 0
+   [:scissors :scissors] 3})
+
+(def points-per-type
+  {:rock 1
+   :paper 2
+   :scissors 3})
+
+(defn calc-outcome [input]
+  (let [[opp me] input
+        opp-type (opp-lookup opp)
+        me-outcome (need-to me)
+        me-should-choose (me-chooser [opp-type me-outcome])
+        battle [opp-type me-should-choose]
+        battle-points (points-outcome battle)
+        me-points (points-per-type me-should-choose)]
+    (+ battle-points me-points)))
+
+(->> parsed
+     (map calc-outcome)
+     (apply +))
