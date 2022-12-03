2f4df177f18f34f7e2a42212724403d144a6fae5
Henry Perschk <henry.perschk@gmail.com>
2022-12-03 16:34:10 +0100

    2022/03

--- src/advent_of_code/2022/03/problem_1.clj
+++ src/advent_of_code/2022/03/problem_1.clj
@@ -6,5 +6,28 @@
   (->> (slurp "./resources/advent_of_code/2022/day_03.txt")
        str/split-lines))
 
+(defn split-in-half [s]
+  (->> s
+       (partition-all (-> (count s) (/ 2)))
+       (map #(apply str %))))
 
+(defn find-item-in-both-comps [s]
+  (let [[a b] (split-in-half s)]
+    (->> b
+         (filter #(.contains a (str %)))
+         first)))
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+(def prios
+  (let [chars (-> (char-range \a \z)
+                  (concat (char-range \A \Z)))
+        chars-len (count chars)]
+    (zipmap chars
+            (->> (iterate inc 1)
+                 (take chars-len)))))
+
+(->> parsed
+     (map #(prios (find-item-in-both-comps %)))
+     (apply +))
--- src/advent_of_code/2022/03/problem_2.clj
+++ src/advent_of_code/2022/03/problem_2.clj
@@ -1,10 +1,29 @@
 (ns advent-of-code.2022.03.problem-2)
 
 (require '[clojure.string :as str])
+(require '[clojure.set :refer [intersection]])
 
 (def parsed
   (->> (slurp "./resources/advent_of_code/2022/day_03.txt")
-       str/split-lines))
+       str/split-lines
+       (partition-all 3)))
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
 
+(def prios
+  (let [chars (-> (char-range \a \z)
+                  (concat (char-range \A \Z)))]
+    (zipmap chars
+            (->> (iterate inc 1)
+                 (take (count chars))))))
 
+(defn find-common [group]
+  (->> group
+       (map set)
+       (apply intersection)
+       first))
+
+(->> parsed
+     (map #(prios (find-common %)))
+     (apply +))
