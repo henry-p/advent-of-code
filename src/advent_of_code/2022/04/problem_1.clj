990c191dc0f63ed08d62fd429fac5a6ea239298b
Henry Perschk <henry.perschk@gmail.com>
2022-12-04 13:03:38 +0100

    2022/04

--- src/advent_of_code/2022/04/problem_1.clj
+++ src/advent_of_code/2022/04/problem_1.clj
@@ -1,10 +1,21 @@
 (ns advent-of-code.2022.04.problem-1)
 
 (require '[clojure.string :as str])
+(require '[clojure.set :refer [superset?]])
 
 (def parsed
   (->> (slurp "./resources/advent_of_code/2022/day_04.txt")
-       str/split-lines))
+       str/split-lines
+       (mapcat #(str/split % #","))
+       (mapcat #(str/split % #"-"))
+       (map #(Integer/parseInt %))
+       (partition 4)))
 
+(defn fully-contained? [row]
+  (let [[a b c d] row
+        fr (set (range a (inc b)))
+        lr (set (range c (inc d)))]
+    (or (superset? fr lr)
+        (superset? lr fr))))
 
-
+(count (filter fully-contained? parsed))
--- src/advent_of_code/2022/04/problem_2.clj
+++ src/advent_of_code/2022/04/problem_2.clj
@@ -1,10 +1,20 @@
 (ns advent-of-code.2022.04.problem-2)
 
 (require '[clojure.string :as str])
+(require '[clojure.set :refer [intersection]])
 
 (def parsed
   (->> (slurp "./resources/advent_of_code/2022/day_04.txt")
-       str/split-lines))
+       str/split-lines
+       (mapcat #(str/split % #","))
+       (mapcat #(str/split % #"-"))
+       (map #(Integer/parseInt %))
+       (partition 4)))
 
+(defn intersects? [row]
+  (let [[a b c d] row
+        fr (set (range a (inc b)))
+        lr (set (range c (inc d)))]
+    (some? (seq (intersection fr lr)))))
 
-
+(count (filter intersects? parsed))
