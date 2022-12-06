161069be8dc8e7b5373b3585d40e66649c0621ce
Henry Perschk <henry.perschk@gmail.com>
2022-12-06 14:30:03 +0100

    2022/06

--- src/advent_of_code/2022/06/problem_1.clj
+++ src/advent_of_code/2022/06/problem_1.clj
@@ -1,10 +1,11 @@
 (ns advent-of-code.2022.06.problem-1)
 
-(require '[clojure.string :as str])
+(defn find-marker [l payload]
+  (->> payload
+       (partition l 1)
+       (map-indexed vector)
+       (filter #(= (count (set (second %))) l))
+       ffirst
+       (+ l)))
 
-(def parsed
-  (->> (slurp "./resources/advent_of_code/2022/day_06.txt")
-       str/split-lines))
-
-
-
+(find-marker 4 (slurp "./resources/advent_of_code/2022/day_06.txt"))
--- src/advent_of_code/2022/06/problem_2.clj
+++ src/advent_of_code/2022/06/problem_2.clj
@@ -1,10 +1,5 @@
 (ns advent-of-code.2022.06.problem-2)
 
-(require '[clojure.string :as str])
+(require '[advent-of-code.2022.06.problem-1 :refer [find-marker]])
 
-(def parsed
-  (->> (slurp "./resources/advent_of_code/2022/day_06.txt")
-       str/split-lines))
-
-
-
+(find-marker 14 (slurp "./resources/advent_of_code/2022/day_06.txt"))
