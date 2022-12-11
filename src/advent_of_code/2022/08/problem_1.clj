1793614a8f324cf254eebbb669838c8160b47c19
Henry Perschk <henry.perschk@gmail.com>
2022-12-11 11:13:54 +0100

    2022/08

--- src/advent_of_code/2022/08/problem_1.clj
+++ src/advent_of_code/2022/08/problem_1.clj
@@ -2,9 +2,29 @@
 
 (require '[clojure.string :as str])
 
-(def parsed
+(def rows
   (->> (slurp "./resources/advent_of_code/2022/day_08.txt")
-       str/split-lines))
+       str/split-lines
+       (map (fn [line] (map (fn [char] (parse-long (str char))) line)))))
 
+(defn transpose [matrix]
+  (apply mapv vector matrix))
 
+(def cols
+  (transpose rows))
 
+(defn tallest? [pos coll]
+  (let [[left [el & right]] (split-at pos coll)]
+    (or (every? #(> el %) left)
+        (every? #(> el %) right))))
+
+(->>
+  (for [row-pos (range (count cols))]
+    (for [col-pos (range (count rows))]
+      (let [row (nth rows col-pos)
+            col (nth cols row-pos)]
+        (or (tallest? row-pos row)
+            (tallest? col-pos col)))))
+  flatten
+  (filter true?)
+  count)
--- src/advent_of_code/2022/08/problem_2.clj
+++ src/advent_of_code/2022/08/problem_2.clj
@@ -2,9 +2,32 @@
 
 (require '[clojure.string :as str])
 
-(def parsed
+(def rows
   (->> (slurp "./resources/advent_of_code/2022/day_08.txt")
-       str/split-lines))
+       str/split-lines
+       (map (fn [line] (map (fn [char] (parse-long (str char))) line)))))
 
+(defn transpose [matrix]
+  (apply mapv vector matrix))
 
+(def cols
+  (transpose rows))
 
+(defn n-viewable [pos coll]
+  (let [[left [el & right]] (split-at pos coll)
+        how-many (fn [el coll]
+                   (let [n-trees (count (take-while #(> el %) coll))]
+                     (if (= n-trees (count coll)) n-trees (inc n-trees))))]
+    [(how-many el (reverse left))
+     (how-many el right)]))
+
+(->>
+  (for [row-pos (range (count cols))]
+    (for [col-pos (range (count rows))]
+      (let [row (nth rows col-pos)
+            col (nth cols row-pos)]
+        (into (n-viewable row-pos row)
+              (n-viewable col-pos col)))))
+  (apply concat)
+  (map #(apply * %))
+  (apply max))
